a45db63accb978303897a14775e92b406e94c7ac
Haur514 <haruka.yoshioka.514@gmail.com>
2022-08-13 14:31:01 +0900

    itemの売れ筋ランキングを取得できるようにしました．

--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.common.ManipulateItemList;
 import com.example.demo.entity.ItemEntity;
 import com.example.demo.repository.ItemRepository;
 import com.google.gson.Gson;
@@ -39,6 +42,22 @@ public class ItemService {
         return "success";
     }
 
+    public String updateItem(
+        String name,
+        String sellingPrice,
+        String costPrice,
+        String grouping,
+        String salesFigure
+    ){
+        ItemEntity itemEntity = itemRepository.findByName(name);
+        if(!sellingPrice.equals("")) itemEntity.setSellingPrice(Integer.parseInt(sellingPrice));
+        if(!costPrice.equals("")) itemEntity.setCostPrice(Integer.parseInt(costPrice));
+        if(!grouping.equals("")) itemEntity.setGrouping(grouping);
+        if(!salesFigure.equals("")) itemEntity.setSalesFigure(Integer.parseInt(salesFigure));
+        itemRepository.save(itemEntity);
+        return "success";
+    }
+
     public String deleteItem(String name){
         try{
             itemRepository.deleteByName(name);
@@ -46,5 +65,10 @@ public class ItemService {
         }catch(Exception e){
             return "failed";
         }
+    }
+
+    public String getItemRanking(){
+        List<ItemEntity> itemEntities = itemRepository.findAll();
+        return new ManipulateItemList().getItemRanking(itemEntities);
     }
 }
