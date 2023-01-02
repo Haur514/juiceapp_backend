e6e620ee9c596d35c90ca9166b0fb13058415070
Haur514 <haruka.yoshioka.514@gmail.com>
2023-01-02 18:50:07 +0900

    chatのtimezoneをAsia/Tokyoにセット

--- script/init/init-chat.sql
+++ script/init/init-chat.sql
@@ -1,6 +1,9 @@
 CREATE TABLE chat (
     id serial primary key,
-    chat varchar(140)
+    chat varchar(140),
+    date timestamp with time zone default (now() at time zone 'jp')
 );
+
+SET SESSION timezone TO 'Asia/Tokyo';
 
 SELECT * FROM chat;
