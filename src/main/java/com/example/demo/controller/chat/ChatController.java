66f93eb1cbffccafcb3c1d819aebfd7e662c4e90
Haur514 <haruka.yoshioka.514@gmail.com>
2023-01-12 11:24:30 +0900

    refactoring

--- src/main/java/com/example/demo/controller/chat/ChatController.java
+++ src/main/java/com/example/demo/controller/chat/ChatController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.controller.chat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
